20662470b199bc48eaee9c19469a71cfd6b5388d
Surya! <thedevmystic@gmail.com>
2026-08-18 15:11:15 +0530

    feat: add link component

---
+++ components/Link/index.tsx
@@ -0,0 +1,42 @@
+/* Link Component */
+
+'use client';
+
+import { ReactNode } from 'react';
+
+import NextLink from 'next/link';
+import type { LinkProps as NextLinkProps } from 'next/link';
+
+import './Link.css';
+
+interface LinkProps extends NextLinkProps {
+  children: ReactNode;
+  variant?: 'underline' | 'no-underline';
+  className?: string;
+}
+
+export default function Link({
+  children,
+  href,
+  variant = 'underline',
+  className: userClassName = '',
+  ...props
+}: LinkProps) {
+  const className = `link ${variant === 'underline' ? 'underline' : 'no-underline'} ${userClassName}`;
+
+  const dispatchEvent = (eventName: string) => {
+    window.dispatchEvent(new CustomEvent(eventName));
+  };
+
+  return (
+    <NextLink
+      href={href}
+      className={className}
+      onMouseEnter={() => dispatchEvent('mouse-hover-start')}
+      onMouseLeave={() => dispatchEvent('mouse-hover-end')}
+      {...props}
+    >
+      {children}
+    </NextLink>
+  );
+}
