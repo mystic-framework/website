e0f9e6d410bd2e22d23fa84fd2515271d630e847
Surya! <thedevmystic@gmail.com>
2026-08-18 15:09:40 +0530

    feat: add button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,67 @@
+/* Button Component */
+
+'use client';
+
+import { useRef } from 'react';
+import type { ButtonHTMLAttributes } from 'react';
+
+import './Button.css';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'ghost' | 'ui';
+  className?: string;
+}
+
+export default function Button({
+  children,
+  className,
+  variant = 'secondary',
+  ...props
+}: ButtonProps) {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const combinedClassName = `button ${variant} ${className}`;
+
+  const handleMouseEnter = () => {
+    if (!buttonRef.current) return;
+
+    const rect = buttonRef.current.getBoundingClientRect();
+    const computedStyle = window.getComputedStyle(buttonRef.current);
+
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const width = rect.width + 2;
+    const height = rect.height + 2;
+    const borderRadius = computedStyle.borderRadius;
+
+    const event = new CustomEvent('mouse-snap-start', {
+      detail: {
+        centerX,
+        centerY,
+        width,
+        height,
+        borderRadius,
+      },
+    });
+
+    window.dispatchEvent(event);
+  };
+
+  const handleMouseLeave = () => {
+    window.dispatchEvent(new CustomEvent('mouse-snap-end'));
+  };
+
+  return (
+    <button
+      ref={buttonRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      type="button"
+      className={combinedClassName}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
